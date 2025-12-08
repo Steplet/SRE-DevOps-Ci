3e011de6768ad970b62e486ba3a06c529a15bb03
Артём Стеберг <asteberg@list.ru>
2025-12-08 21:07:37 +0300

    linter fix

--- backend/cats/models.py
+++ backend/cats/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-
 User = get_user_model()
 
 
@@ -17,23 +16,28 @@ class Cat(models.Model):
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
     owner = models.ForeignKey(
-        User, related_name='cats', 
-        on_delete=models.CASCADE
-        )
-    achievements = models.ManyToManyField(Achievement, through='AchievementCat')
+        User,
+        related_name="cats",
+        on_delete=models.CASCADE)
+
+    achievements = models.ManyToManyField(
+        Achievement,
+        through="AchievementCat")
     image = models.ImageField(
-        upload_to='cats/images/', 
-        null=True,  
-        default=None
-        )
+        upload_to="cats/images/",
+        null=True, default=None)
 
     def __str__(self):
         return self.name
 
 
 class AchievementCat(models.Model):
-    achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
-    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+    achievement = models.ForeignKey(
+        Achievement,
+        on_delete=models.CASCADE)
+    cat = models.ForeignKey(
+        Cat,
+        on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.achievement} {self.cat}'
+        return f"{self.achievement} {self.cat}"
